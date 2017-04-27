08cb1b55561ad5ae2f27cbe5c5f12de97a139d9e
Etudiant <omar.hamzi@lepoles.com>
2017-04-27 17:04:10 +0200

    maj formualire site

--- Ecommerce/inscription.php
+++ Ecommerce/inscription.php
@@ -3,32 +3,32 @@ require_once('inc/init.php');
 require_once("inc/haut_inc.php");
 
 ?>
-<form method="post" action="">
+<form action="" method="post">
+    <label for="pseudo">Pseudo</label><br>
+    <input type="text" id="pseudo" name="pseudo" maxlength="20" placeholder="Votre pseudo" pattern="[a-zA-Z0-9-_.] {3,20}" title="caractères acceptés : a-zA-Z0-9-_." required="required"><br><br>
 
-<label for="pseudo">Pseudo</label><br>
-<input name="pseudo" type="text" max-length ='20' id="pseudo" placehorder=" Votre pseudo"
-pattern='[a-zA-Z0-9-_.]{3,20}' title="Caractères acceptés : a-zA-Z0-9-_." required="required"><br><br>
-<label for="mdp">Mot de Passe</label><br>
-<input name="mdp" type="password" id="mdp" required><br><br>
-<input name="mdp" type="password" id="mdp" required><br><br>
-<input name="mdp" type="password" id="mdp" required><br><br>
-<input name="mdp" type="password" id="mdp" required><br><br>
+    <label for="mdp">Mot de passe</label><br>
+    <input type="password" id="mdp" name="mdp" required="required"><br><br>
 
-<label for="nom">Nom</label><br>
-<input name="nom" type="text" id="nom" placehorder="Votre nom"><br><br>
+    <label for="nom">Nom</label><br>
+    <input type="text" id="nom" name="nom" placeholder="Votre nom"><br><br>
 
-<label for="prenom">Prénom</label><br>
-<input name="prenom" type="text" id="prenom" placehorder="Votre prénom"><br><br>
+    <label for="prenom">Prénom</label><br>
+    <input type="text" id="prenom" name="prenom" placeholder="Votre prénom"><br><br>
 
-<label for="email">Email</label><br>
-<input name="email" type="email" id="email"><br><br>
+    <label for="email">Email</label><br>
+    <input type="email" id="email" name="email" placeholder="exemple@gmail.com"><br><br>
 
+    <label for="civilite">Civilité</label><br>
+    <input type="radio" id="civilite" name="civilite" value="m" checked>Homme
+    <input type="radio" id="civilite" name="civilite" value="f">Femme<br><br>
 
-<label for="mdp">Mot de Passe</label><br>
-<label for="mdp">Mot de Passe</label><br>
-<label for="mdp">Mot de Passe</label><br>
-<label for="mdp">Mot de Passe</label><br>
+    <label for="ville">Ville</label><br>
+    <input type="text" id="ville" name="ville" placeholder="Votre ville" pattern="[a-zA-Z0-9-_.] {3,20}" title="caractères acceptés : a-zA-Z0-9-_." required="required"><br><br>
 
+    <label for="code_postal">Code postal</label><br>
+    <input type="text" id="code_postal" name="code_postal" placeholder="Votre code postal" pattern="[0-9] {5}" title="caractères acceptés : [0-9]"><br><br>
+</form>
 
 
 <?php
